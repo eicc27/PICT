fca0b8947338074a6280954015a5e06aa3d1e181
Sihan Chen <13917164365@163.com>
2022-08-01 21:58:23 +0800

    Added Windows browser sniffer

--- back/utils/BrowserChecker.ts
+++ back/utils/BrowserChecker.ts
@@ -21,6 +21,12 @@ export default class BrowserChecker {
         Browsers.Edge,
         Browsers.Chromium,
     ];
+
+    static programFilesDir = {
+        x64: 'C:\\Program Files\\',
+        x86: 'C:\\Program Files (x86)\\'
+    }
+
     static defaultDir = {
         windows: `${process.env.USERPROFILE}\\AppData\\Local\\ms-playwright`,
         linux: `${process.env.HOME}/.cache/ms-playwright`,
@@ -41,12 +47,12 @@ export default class BrowserChecker {
     private getPlaywrightBrowsers(): unknown[] {
         let dir = '';
         switch (this.os) {
-        case 'win32':
-            dir = BrowserChecker.defaultDir.windows;
-            break;
-        case 'linux':
-            dir = BrowserChecker.defaultDir.linux;
-            break;
+            case 'win32':
+                dir = BrowserChecker.defaultDir.windows;
+                break;
+            case 'linux':
+                dir = BrowserChecker.defaultDir.linux;
+                break;
         }
         if (!dir) {
             return [];
@@ -70,9 +76,53 @@ export default class BrowserChecker {
      */
     private async getSystemBrowsers() {
         switch (this.os) {
-        case 'linux':
-            return await this.getBrowsersForLinux();
+            case 'linux':
+                return await this.getBrowsersForLinux();
+            case 'win32':
+                return await this.getBrowersForWindows();
         }
+    }
+
+    private pushAvailableBrowsersOnWindows(browsers: unknown[], browserVendorName: string, browserLocation: String) {
+        if (browserLocation.includes(browserVendorName))
+            browsers.push({
+                browser: browserVendorName.toLowerCase(),
+                type: 'system',
+            });
+    }
+
+    private async getBrowersForWindows() {
+        const browserLocations = ['Google\\Chrome', 'Mozilla Firefox', 'Microsoft\\Edge'];
+        let availableLocations: string[] = [];
+        browserLocations.forEach(location => {
+            availableLocations.push(BrowserChecker.programFilesDir.x86.concat(location),
+                BrowserChecker.programFilesDir.x64.concat(location));
+        });
+        const browsers: unknown[] = [];
+        for (let i = 0; i < availableLocations.length; i++) {
+            const location = availableLocations[i];
+            let result = await this.getBrowserForWindows(location);
+            if (result) {
+                this.pushAvailableBrowsersOnWindows(browsers, 'Chrome', location);
+                this.pushAvailableBrowsersOnWindows(browsers, 'Firefox', location);
+                this.pushAvailableBrowsersOnWindows(browsers, 'Edge', location);
+            }
+            
+        }
+        return browsers;
+    }
+
+    private async getBrowserForWindows(browserLocation: string): Promise<boolean> {
+        return new Promise(
+            (resolve) => {
+                exec(`cd ${browserLocation}`, (err) => {
+                    if (!err)
+                        resolve(true);
+                    else
+                        resolve(false);
+                })
+            }
+        )
     }
 
     private async getBrowsersForLinux(): Promise<unknown[]> {
